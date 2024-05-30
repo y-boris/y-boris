33e7ecfa96f3f608d782fc083f86c130a6f7cf3b
Boris Yarmulnik <boris.yarmoulnik@gmail.com>
2024-05-30 18:41:12 +0300

    feat: hamster auto clicker

---
+++ hamster-kombat/clicker.js
@@ -0,0 +1,19 @@
+// chrome extension https://chromewebstore.google.com/detail/resource-override/pkoacgokdfckfpndoffpifphamojphii
+
+(function () {
+  const evt1 = new PointerEvent('pointerdown', { clientX: 150, clientY: 300 });
+  const evt2 = new PointerEvent('pointerup', { clientX: 150, clientY: 300 });
+  setInterval(
+    (function fn() {
+      const energy = parseInt(
+        document.getElementsByClassName('user-tap-energy')[0].getElementsByTagName('p')[0].textContent.split(' / ')[0]
+      );
+      if (energy > 25) {
+        document.getElementsByClassName('user-tap-button')[0].dispatchEvent(evt1);
+        document.getElementsByClassName('user-tap-button')[0].dispatchEvent(evt2);
+      }
+      return fn;
+    })(),
+    50
+  );
+})();
